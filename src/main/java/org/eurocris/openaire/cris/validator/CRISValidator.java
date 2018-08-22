ba2aa98577166cd6169b6d9fc827a3caf5d59548
Jan Dvorak <jdvorak001@users.noreply.github.com>
2018-08-22 10:10:52 +0200

    ignore XML Schema regular expression matching errors
    - OAI-PMH metadata payload validation postponed to DOM
      i.e., no longer at parse time
    - ignore any "cvc-pattern-valid" error
      and a following "cvc-complex-type.2.2"
    - two schema singletons (parserSchema and validatorSchema)
      instead of just the parserSchema
    - SchemaFactory as yet another singleton

--- src/main/java/org/eurocris/openaire/cris/validator/CRISValidator.java
+++ src/main/java/org/eurocris/openaire/cris/validator/CRISValidator.java
@@ -30,9 +30,11 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.Source;
+import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamSource;
 import javax.xml.validation.Schema;
 import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Validator;
 
 import org.apache.commons.cli.MissingArgumentException;
 import org.eurocris.openaire.cris.validator.OAIPMHEndpoint.ConnectionStreamFactory;
@@ -56,7 +58,9 @@ import org.openarchives.oai._2.StatusType;
 import org.openarchives.oai._2_0.oai_identifier.OaiIdentifierType;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.xml.sax.ErrorHandler;
 import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
 
 /**
  * Validating a given OAI-PMH endpoint for compliance with the OpenAIRE Guidelines for CRIS Managers 1.1.
@@ -135,7 +139,7 @@ public class CRISValidator {
 	public static void main( final String[] args ) throws Exception {
 		final String endpointUrl = ( args.length > 0 ) ? args[0] : null;
 		final URL endpointBaseUrl = new URL( endpointUrl );
-		endpoint = new OAIPMHEndpoint( endpointBaseUrl, VALIDATION_MODE, createParserSchema(), CONN_STREAM_FACTORY );
+		endpoint = new OAIPMHEndpoint( endpointBaseUrl, VALIDATION_MODE, getParserSchema(), CONN_STREAM_FACTORY );
 		JUnitCore.main( CRISValidator.class.getName() );
 	}
 	
@@ -155,7 +159,7 @@ public class CRISValidator {
 				throw new MissingArgumentException( "Please specify the OAI-PMH endpoint URL as the value of the " + endpointPropertyKey + " system property or as the first argument on the command line" );
 			}
 			final URL endpointBaseUrl = new URL( endpointUrl );
-			endpoint = new OAIPMHEndpoint( endpointBaseUrl, VALIDATION_MODE, createParserSchema(), CONN_STREAM_FACTORY );			
+			endpoint = new OAIPMHEndpoint( endpointBaseUrl, VALIDATION_MODE, getParserSchema(), CONN_STREAM_FACTORY );			
 		}
 	}
 	
@@ -166,27 +170,55 @@ public class CRISValidator {
 		return endpoint.getBaseUrl();
 	}
 	
-	private static Schema schema = null;
+	private static Schema parserSchema = null;
 	
 	/**
 	 * Create the schema for the validating XML parser.
 	 * @return the compound schema
 	 * @throws SAXException when problem reading the schema
 	 */
-	protected static synchronized Schema createParserSchema() throws SAXException {
-		if ( schema == null ) {
-			final SchemaFactory sf = SchemaFactory.newInstance( W3C_XML_SCHEMA_NS_URI );
-			final Source[] schemas = { 
+	protected static synchronized Schema getParserSchema() throws SAXException {
+		if ( parserSchema == null ) {
+			parserSchema = getXmlSchemaFactory().newSchema( new Source[] { 
 					schema( "/cached/oai-identifier.xsd" ), 
 					schema( "/cached/OAI-PMH.xsd" ), 
 					schema( "/cached/oai_dc.xsd" ), 
 					schema( "/cached/xml.xsd", "http://www.w3.org/2001/xml.xsd" ), 
-				};
-			schema = sf.newSchema( schemas );
+				} );
 		}
-		return schema;
+		return parserSchema;
 	}
 
+	private static Schema validatorSchema = null;
+
+	/**
+	 * Create the schema for the second-phase validation.
+	 * @return the compound schema
+	 * @throws SAXException when problem reading the schema
+	 */
+	protected static synchronized Schema getValidatorSchema() {
+		if ( validatorSchema == null ) {
+			try {
+				validatorSchema = getXmlSchemaFactory().newSchema( new Source[] { 
+						schema( "/openaire-cerif-profile.xsd" ), 
+						schema( "/cached/xml.xsd", "http://www.w3.org/2001/xml.xsd" ), 
+					} );
+			} catch ( final SAXException e ) {
+				throw new IllegalStateException( "While initializing validator schema", e );
+			}
+		}
+		return validatorSchema;
+	}
+
+	private static SchemaFactory xmlSchemaFactory = null;
+	
+	private static synchronized SchemaFactory getXmlSchemaFactory() {
+		if ( xmlSchemaFactory == null ) {
+			xmlSchemaFactory = SchemaFactory.newInstance( W3C_XML_SCHEMA_NS_URI );
+		}
+		return xmlSchemaFactory;
+	}
+	
 	private static Source schema( final String path ) {
 		return schema( path, null );
 	}
@@ -203,7 +235,7 @@ public class CRISValidator {
 		return src;
 	}
 
-	@SuppressWarnings( "unused")
+	@SuppressWarnings( "unused" )
 	private static Optional<String> sampleIdentifier = Optional.empty();
 
 	private static Optional<String> serviceAcronym = Optional.empty();
@@ -243,6 +275,7 @@ public class CRISValidator {
 					final Element el = (Element) obj;
 					if ( "Service".equals( el.getLocalName() ) && OPENAIRE_CERIF_XMLNS.equals( el.getNamespaceURI() ) ) {
 						serviceAcronym = XmlUtils.getTextContents( XmlUtils.getFirstMatchingChild( el, "Acronym", el.getNamespaceURI() ) );
+						validateMetadataPayload( el );
 						return true;
 					}
 				}
@@ -502,6 +535,7 @@ public class CRISValidator {
 						final Element el = (Element) obj;
 						assertEquals( "The payload element not in the right namespace", expectedQName.getNamespaceURI(), el.getNamespaceURI() );
 						assertEquals( "The payload element does not have the right local name", expectedQName.getLocalPart(), el.getLocalName() );
+						validateMetadataPayload( el );
 						final CERIFNode node = CERIFNode.buildTree( el );
 						recordsByName.put( node.getName(), node );
 						recordsByOaiIdentifier.put( t.getHeader().getIdentifier(), node );
@@ -556,6 +590,50 @@ public class CRISValidator {
 				final CERIFNode missingNode = node.reportWhatIMiss( baseNode ).get();
 				fail( "Violation of (5b) in " + oaiIdentifier + ":\n" + node + "is not subset of\n" + baseNode + "missing is\n" + missingNode );
 			}
+		}
+	}
+
+	/**
+	 * Validate the metadata payload subtree against the XML Schema resulting from {@link #getValidatorSchema()}.
+	 * @param el the metadata payload top element
+	 */
+	protected void validateMetadataPayload( final Element el ) {
+		final String elString = el.getLocalName() + "[@id=\"" + el.getAttribute( "id" ) + "\"]";
+		final Validator validator = getValidatorSchema().newValidator();
+		try {
+			final ErrorHandler errorHandler = new ErrorHandler() {
+				
+				private boolean patternValidErrorSignalled = false;
+				
+				@Override
+				public void warning( final SAXParseException exception ) throws SAXException {
+					// do nothing
+				}
+				
+				@Override
+				public void fatalError( final SAXParseException exception ) throws SAXException {
+					throw exception;
+				}
+				
+				@Override
+				public void error( final SAXParseException exception ) throws SAXException {
+					final String msg = exception.getMessage();
+					if ( msg.startsWith( "cvc-pattern-valid: " ) ) {
+						patternValidErrorSignalled = true;
+						System.err.println( "In " + elString + ": " + msg );
+					} else {
+						if (!( patternValidErrorSignalled && msg.startsWith( "cvc-complex-type.2.2: " ) )) {
+							throw exception;
+						}
+						patternValidErrorSignalled = false;
+					}
+				}
+
+			};
+			validator.setErrorHandler( errorHandler );
+			validator.validate( new DOMSource( el ) );
+		} catch ( final SAXException | IOException e ) {
+			fail( "While validating element " + elString + ": " + e );
 		}
 	}
 	
